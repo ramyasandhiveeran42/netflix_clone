6aeccf5ceff58eee5fb2d7dc7eb9432a8347c28d
Ramya <ramyasandhiveeran42@gmail.com>
2024-11-03 01:02:24 +0530

    updated login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import './login.css'
+import './Login.css'
 import logo from '../../assets/logo.png'
 import { Link } from 'react-router-dom'
 import { login, signUp } from '../../firebase'
